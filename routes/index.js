bcd8f0ea390ef42995b8072f16044ed43d0e3560
NikshepAV <nikshep.av@gmail.com>
2018-03-16 23:50:56 +0530

    Auth ready

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var express		=	require('express'),
 	User 		= 	require('../models/user'),
 	async		=	require('async'),
 	nodemailer 	=	require('nodemailer'),
-	crypto 		=	require('crypto');
+	crypto 		=	require('crypto'),
+	middleware  = 	require('../middleware');
 
 // render the landing page
 router.get('/', function(req, res) {
@@ -88,54 +89,54 @@ router.get('/forgot', function(req, res) {
 
 // handle the forgot password logic
 router.post('/forgot', function(req, res, next) {
-  async.waterfall([
-    function(done) {
-      crypto.randomBytes(20, function(err, buf) {
-        var token = buf.toString('hex');
-        done(err, token);
-      });
-    },
-    function(token, done) {
-      User.findOne({ email: req.body.email }, function(err, user) {
-        if (!user) {
-          req.flash('error', 'No account with that email address exists.');
-          return res.redirect('/forgot');
-        }
+	async.waterfall([
+		function(done) {
+			crypto.randomBytes(20, function(err, buf) {
+				var token = buf.toString('hex');
+				done(err, token);
+			});
+		},
+		function(token, done) {
+			User.findOne({ email: req.body.email }, function(err, user) {
+				if (!user) {
+					req.flash('error', 'No account with that email address exists.');
+					return res.redirect('/forgot');
+				}
 
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+				user.resetPasswordToken = token;
+				user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
-        user.save(function(err) {
-          done(err, token, user);
-        });
-      });
-    },
-    function(token, user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail', 
-        auth: {
-          user: 'campus.io.mailer@gmail.com',
-          pass: 'password goes here'
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'campus.io.mailer@gmail.com',
-        subject: 'Node.js Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
-    }
-  ], function(err) {
-    if (err) return next(err);
-    res.redirect('/forgot');
-  });
+				user.save(function(err) {
+					done(err, token, user);
+				});
+			});
+		},
+		function(token, user, done) {
+			var smtpTransport = nodemailer.createTransport({
+				service: 'Gmail', 
+				auth: {
+					user: 'campus.io.mailer@gmail.com',
+					pass: 'password goes here'
+				}
+			});
+			var mailOptions = {
+				to: user.email,
+				from: 'campus.io.mailer@gmail.com',
+				subject: 'CAMPUS.IO Password Reset',
+				text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account at CAMPUS.IO.\n\n' +
+				'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+				'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+				'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+			};
+			smtpTransport.sendMail(mailOptions, function(err) {
+				req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+				done(err, 'done');
+			});
+		}
+	], function(err) {
+		if (err) return next(err);
+		res.redirect('/forgot');
+	});
 });
 
 // render the reset password page
@@ -188,7 +189,7 @@ router.post('/reset/:token', function(req, res) {
 				from: 'campus.io.mailer@gmail.com',
 				subject: 'Your password has been changed',
 				text: 'Hello,\n\n' +
-					  'This is a confirmation that the password for your account at Campus.IO has just been changed.'
+					  'This is a confirmation that the password for your account at CAMPUS.IO has just been changed.'
 			};
 			smtpTransport.sendMail(mailOptions, function(err) {
 				req.flash('success', 'Password successfully changed');
@@ -202,7 +203,7 @@ router.post('/reset/:token', function(req, res) {
 });
 
 // render the home page
-router.get('/home', function(req, res) {
+router.get('/home', middleware.isLoggedIn, function(req, res) {
 	res.render('home');
 });
 
