b2d4c5a0105f43521333fb956df4af29a8468203
NikshepAV <nikshep.av@gmail.com>
2018-05-26 13:11:51 +0530

    Remove home tab from navbar & add animations for dropdowns

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -36,3 +36,13 @@ function toggleGraph(id) {
 	$("#" + id).toggle("slow", "swing", function() {
 	});
 }
+
+// Add slideDown animation to Bootstrap dropdown when expanding.
+$('.dropdown').on('show.bs.dropdown', function() {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
